e789d59022778575700f8f4eb144cd7368ff0ca8
Elena Rivas <rivas.cuartero@gmail.com>
2015-01-07 17:07:59 +0000

    git-svn-id: https://svn.janelia.org/eddylab/rivase/src@1118 ce6924e9-e403-0410-acfd-8bc30e2e5733

--- src/mutualinfo.c
+++ src/mutualinfo.c
@@ -7,10 +7,8 @@
 #include <string.h>
 
 #include "easel.h"
-#include "esl_distance.h"
-#include "esl_fileparser.h"
+#include "esl_dmatrix.h"
 #include "esl_msa.h"
-#include "esl_msaweight.h"
 #include "esl_vectorops.h"
 #include "esl_wuss.h"
 
@@ -18,3 +16,42 @@
 #include "ratematrix.h"
 #include "ribosum_matrix.h"
 
+int                 
+Mutual_Calculate(ESL_MSA *msa, struct mutual_s *mi, double tol, int verbose, char *errbuf)
+{
+  return eslOK;
+}
+
+struct mutual_s *
+Mutual_Create(int64_t alen)
+{
+  struct mutual_s *mi = NULL;
+  int              status;
+
+  ESL_ALLOC(mi, sizeof(struct mutual_s));
+  mi->alen = alen;
+  
+  mi->MI  = esl_dmatrix_Create(alen, alen);
+  mi->MIa = esl_dmatrix_Create(alen, alen);
+  mi->MIp = esl_dmatrix_Create(alen, alen);
+  mi->MIr = esl_dmatrix_Create(alen, alen);
+  ESL_ALLOC(mi->H, sizeof(double) * alen);
+ 
+  return mi;
+
+ ERROR:
+  return NULL;
+}
+
+void                
+Mutual_Destroy(struct mutual_s *mi)
+{
+  if (mi) {
+    esl_dmatrix_Destroy(mi->MI);
+    esl_dmatrix_Destroy(mi->MIa);
+    esl_dmatrix_Destroy(mi->MIp);
+    esl_dmatrix_Destroy(mi->MIr);
+    free(mi->H);
+    free(mi);
+  }
+}
--- src/mutualinfo.h
+++ src/mutualinfo.h
@@ -11,8 +11,22 @@
 
 
 #include "easel.h"
-#include "esl_alphabet.h"	/* ESL_DSQ, ESL_ALPHABET */
-#include "esl_dmatrix.h"	/* ESL_DMATRIX           */
+#include "esl_dmatrix.h"
+#include "esl_msa.h"
 
+
+struct mutual_s {
+  int64_t      alen;
+  ESL_DMATRIX *MI;     // MI  mutual information
+  ESL_DMATRIX *MIa;    // MIa mutual information
+  ESL_DMATRIX *MIp;    // MIp mutual information
+  ESL_DMATRIX *MIr;    // MIr mutual information
+  double      *H;      // entropy per position
+ 
+};
+
+extern int              Mutual_Calculate(ESL_MSA *msa, struct mutual_s *mi, double tol, int verbose, char *errbuf);
+extern struct mutual_s *Mutual_Create(int64_t alen);
+extern void             Mutual_Destroy(struct mutual_s *mi);
 
 #endif
