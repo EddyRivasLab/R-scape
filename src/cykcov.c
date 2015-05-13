3ee8fdb7e905ddb867f41f869e434cb9c969b3b7
Elena Rivas <rivas.cuartero@gmail.com>
2015-05-13 02:50:03 +0000

    git-svn-id: https://svn.janelia.org/eddylab/rivase/src@1245 ce6924e9-e403-0410-acfd-8bc30e2e5733

---
+++ src/cykcov.c
@@ -0,0 +1,26 @@
+/* cykcov.c */
+
+#include "p7_config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "hmmer.h"
+
+#include "easel.h"
+#include "esl_alphabet.h"
+#include "esl_dmatrix.h"
+#include "esl_msa.h"
+#include "esl_stack.h"
+#include "esl_stats.h"
+#include "esl_tree.h"
+#include "esl_vectorops.h"
+#include "esl_wuss.h"
+
+#include "cykcov.h"
+
+int
+CYKCOV_Fill() {
+  return eslOK;
+}
---
+++ src/cykcov.h
@@ -0,0 +1,21 @@
+/* cykcov.h
+ *
+ *   
+*/
+#ifndef CYKCOV_INCLUDED
+#define CYKCOV_INCLUDED
+
+#include "p7_config.h"
+
+#include <stdio.h>		/* FILE */
+
+
+#include "easel.h"
+#include "esl_alphabet.h"
+#include "esl_dmatrix.h"
+#include "esl_msa.h"
+
+extern int CYKCOV_Fill();
+
+
+#endif
