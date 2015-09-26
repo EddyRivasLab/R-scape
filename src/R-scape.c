9af9c4ac8ba71a008f10cea0dab456f02a7a6a85
Elena Rivas <rivas.cuartero@gmail.com>
2015-09-25 20:44:59 -0400

    one more tweek

--- src/R-scape.c
+++ src/R-scape.c
@@ -910,6 +910,7 @@ calculate_width_histo(ESL_GETOPTS *go, struct cfg_s *cfg, ESL_MSA *msa)
   if (mi->maxCOV <= cfg->bmin) ESL_XFAIL(eslFAIL, cfg->errbuf, "bmin %f should be larger than maxCOV %f\n", cfg->bmin, mi->maxCOV);
 
   cfg->w = (mi->maxCOV - ESL_MAX(data.bmin,mi->minCOV)) / (double) cfg->hpts;
+  if (cfg->w < cfg->tol) cfg->w = cfg->tol;
   if (1||cfg->verbose) printf("w %f minCOV %f bmin %f maxCOV %f\n", cfg->w, mi->minCOV, cfg->bmin, mi->maxCOV);
   if (cfg->w <= 0) return eslFAIL;
 
