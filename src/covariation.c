420073d7e3be502565973a78aaa92a15520949ac
Elena Rivas <rivas.cuartero@gmail.com>
2016-01-15 08:44:48 -0500

    minor changes

--- src/covariation.c
+++ src/covariation.c
@@ -70,7 +70,6 @@ cov_Calculate(struct data_s *data, ESL_MSA *msa, RANKLIST **ret_ranklist, HITLIS
     status = cov_Probs(data->r, msa, data->T, data->ribosum, data->mi, data->method, data->donull2b, data->tol, data->verbose, data->errbuf);
     if (status != eslOK) goto ERROR;
   }
-
   switch(data->covtype) {
   case CHIa: 
     status = cov_CalculateCHI         (covclass, data, FALSE,   NULL,      NULL);
@@ -2492,7 +2491,7 @@ cov_PlotHistogramSurvival(struct data_s *data, char *gnuplot, char *covhisfile, 
     if (status != eslOK) goto ERROR;
   }
   linespoints = TRUE; 
-#if 1
+#if 0
   status = cov_histogram_plotexpectsurv  (pipe, ranklist->ha->Nc, ranklist->ht, key2, posx, posy-8*incy,        FALSE, 1, linespoints, 44, 2);
   if (status != eslOK) goto ERROR;
 #endif
