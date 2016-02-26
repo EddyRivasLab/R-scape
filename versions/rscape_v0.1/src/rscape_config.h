c59a9ab3c6141a551ce0fda45211c2b187b46098
Elena Rivas <erivas@dhcp-140-247-92-106.fas.harvard.edu>
2016-02-26 16:14:11 -0500

    R-scape

--- versions/rscape_v0.1/src/rscape_config.h
+++ versions/rscape_v0.1/src/rscape_config.h
@@ -1,4 +1,4 @@
-/* src/rscape_config.h.  Generated from rscape_config.h.in by configure.  */
+/* rscape_config.h.  Generated from rscape_config.h.in by configure.  */
 /* rscape_config.h.in  [input to configure]
  * 
  * System-dependent configuration of Easel, by autoconf.
@@ -14,10 +14,11 @@
 
 /* Version info.
  */
-#define RSCAPE_VERSION "0.1"
-#define RSCAPE_DATE "FEB 2016"
-#define RSCAPE_COPYRIGHT "Copyright (C) 2016 Howard Hughes Medical Institute."
-#define RSCAPE_LICENSE "Freely distributed under the GNU General Public License (GPLv3)."
+#define RSCAPE_VERSION ""
+#define RSCAPE_DATE ""
+#define RSCAPE_COPYRIGHT ""
+#define RSCAPE_LICENSE ""
+#define RSCAPE_HOME "/Users/erivas/src/src/mysource/versions/rscape_v0.1"
 
 /* Large file support
  * Must precede any header file inclusion.
@@ -39,12 +40,12 @@
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_STRINGS_H 1
 
-/* #undef HAVE_SYS_PARAM_H */
+#define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_SYSCTL_H 1
 
-/* #undef HAVE_EMMINTRIN_H */
-/* #undef HAVE_PMMINTRIN_H */
-/* #undef HAVE_XMMINTRIN_H */
+#define HAVE_EMMINTRIN_H 1
+#define HAVE_PMMINTRIN_H 1
+#define HAVE_XMMINTRIN_H 1
 
 /* #undef HAVE_ALTIVEC_H */
 
@@ -67,33 +68,33 @@
 
 /* Optional parallel implementation support
  */
-/* #undef HAVE_SSE2 */
+#define HAVE_SSE2 1
 /* #undef HAVE_VMX */
 /* #undef HAVE_MPI */
 #define HAVE_PTHREAD 1
 
-#define HAVE_SSE2_CAST 1
+/* #undef HAVE_SSE2_CAST */
 
 /* Programs */
 #define HAVE_GZIP 1
 
 /* Functions */
-/* #undef HAVE_CHMOD */
+#define HAVE_CHMOD 1
 #define HAVE_FSEEKO 1
 #define HAVE_FSTAT 1
 #define HAVE_GETCWD 1
 #define HAVE_GETPID 1
 #define HAVE_MKSTEMP 1
 #define HAVE_POPEN 1
-/* #undef HAVE_PUTENV */
+#define HAVE_PUTENV 1
 #define HAVE_STAT 1
 #define HAVE_STRCASECMP 1
 #define HAVE_SYSCONF 1
 #define HAVE_SYSCTL 1
 #define HAVE_TIMES 1
-/* #undef HAVE_ERFC */
+#define HAVE_ERFC 1
 
-/* #undef HAVE_FUNC_ATTRIBUTE_NORETURN */
+#define HAVE_FUNC_ATTRIBUTE_NORETURN 1
 
 
 #endif /*rscapeCONFIG_INCLUDED*/
