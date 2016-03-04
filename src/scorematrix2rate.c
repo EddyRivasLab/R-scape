6f827d011abd0055607ae68664d461b4a9c1d25a
Elena Rivas <erivas@dhcp-140-247-92-106.fas.harvard.edu>
2016-03-04 10:27:25 -0500

    R-scape

--- src/scorematrix2rate.c
+++ src/scorematrix2rate.c
@@ -77,7 +77,7 @@ process_commandline(int argc, char **argv, ESL_GETOPTS **ret_go, char **ret_qfil
 static int
 output_header(FILE *ofp, ESL_GETOPTS *go)
 {
-  easel_banner(ofp, go->argv[0], banner);
+  esl_banner(ofp, go->argv[0], banner);
   
   if (esl_opt_IsUsed(go, "-o")          && fprintf(ofp, "# output directed to file:         %s\n",             esl_opt_GetString(go, "-o"))          < 0) ESL_EXCEPTION_SYS(eslEWRITE, "write failed");
   if (esl_opt_IsUsed(go, "--mx")        && fprintf(ofp, "# subst score matrix (built-in):   %s\n",             esl_opt_GetString(go, "--mx"))        < 0) ESL_EXCEPTION_SYS(eslEWRITE, "write failed");
