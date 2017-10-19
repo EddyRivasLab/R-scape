7a5dbe8d91c7597afa82f6f138127e137329bda6
Elena Rivas <erivas@eddyfs01.rc.fas.harvard.edu>
2017-10-19 10:54:56 -0400

    potts

--- src/pottsbuild.c
+++ src/pottsbuild.c
@@ -260,6 +260,7 @@ potts_Build(ESL_RANDOMNESS *r, ESL_MSA *msa, double ptmuh, double ptmue, PTTRAIN
    ESL_XFAIL(eslFAIL, errbuf, "error, you should not be here");
      break;
   case PLM:
+    // follows gremling_v2
     pt->muh *= msa->alen; // scaled by length
     pt->mue *= msa->alen;
 
@@ -267,11 +268,15 @@ potts_Build(ESL_RANDOMNESS *r, ESL_MSA *msa, double ptmuh, double ptmue, PTTRAIN
     if (status != eslOK) ESL_XFAIL(eslFAIL, errbuf, "error all optimizing potts");
     break;
   case APLM:
+    // follows plmDCA_asymmetric_v2
     neff = esl_vec_DSum(msa->wgt, msa->nseq);
     if (neff < 500) { // scaled by number of sequences
       pt->muh = 0.1 - (0.1-0.01)*neff/500; 
       pt->mue = pt->muh;
     }
+    pt->muh *= neff; // scaled by neff
+    pt->mue *= neff/2.;
+    
 
     status = potts_OptimizeCGD_APLM(pt, msa, tol, errbuf, verbose);
     //status = potts_OptimizeLBFGS_APLM(pt, msa, tol, errbuf, verbose);
@@ -932,9 +937,11 @@ optimize_aplm_unpack_paramvector(double *p, int np, struct optimize_data *data)
   
   for (a = 0; a < Kg; a++)                            data->pt->h[i][a]              = p[x++];
   for (j = 0;   j < i; j++) 
-    for (a = 0; a < Kg; a++) for (b = 0; b < Kg; b++) data->pt->e[i][j][IDX(a,b,Kg)] = data->pt->e[j][i][IDX(b,a,Kg)] = p[x++]; 
+    for (a = 0; a < Kg; a++) for (b = 0; b < Kg; b++) data->pt->e[i][j][IDX(a,b,Kg)] = p[x++]; 
+  //for (a = 0; a < Kg; a++) for (b = 0; b < Kg; b++) data->pt->e[i][j][IDX(a,b,Kg)] = data->pt->e[j][i][IDX(b,a,Kg)] = p[x++]; 
   for (j = i+1; j < L; j++) 
-    for (a = 0; a < Kg; a++) for (b = 0; b < Kg; b++) data->pt->e[i][j][IDX(a,b,Kg)] = data->pt->e[j][i][IDX(b,a,Kg)] = p[x++];
+    for (a = 0; a < Kg; a++) for (b = 0; b < Kg; b++) data->pt->e[i][j][IDX(a,b,Kg)] = p[x++];
+  //for (a = 0; a < Kg; a++) for (b = 0; b < Kg; b++) data->pt->e[i][j][IDX(a,b,Kg)] = data->pt->e[j][i][IDX(b,a,Kg)] = p[x++];
 
   return eslOK;
 
